e08d3e9df0fb0127bdb42be848081194a9a6a25d
Saad <saadkamran157@gmail.com>
2023-01-16 11:02:51 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+    navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+    navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+    navLinks.classList.toggle("show3");
+}
+
+let homeArrow = document.querySelector(".home-arrow");
+homeArrow.onclick = function () {
+    navLinks.classList.toggle("show4");
+
+}
